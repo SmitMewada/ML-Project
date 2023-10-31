00278f400893a72dd8d46dee7f2e2c542ee71a68
Smit Mewada <smitmewada009@gmail.com>
2023-10-31 10:51:35 -0400

    Changes to Data tranformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -30,7 +30,7 @@ class DataTranformation:
             numerical_features = ["writing_score", "reading_score"]
             categorical_features = [
                 "gender",
-                "race_ethnicity",
+                "race/ethnicity",
                 "parental_level_of_education",
                 "lunch",
                 "test_preparation_course"
@@ -47,9 +47,8 @@ class DataTranformation:
             
             categorical_pipeline = Pipeline(
                steps= [
-                   ("imputer", SimpleImputer(strategy="most-frequent")),
-                   ("ohencoder", OneHotEncoder()),
-                   ("scaler", StandardScaler())
+                   ("imputer", SimpleImputer(strategy="most_frequent")),
+                   ("ohencoder", OneHotEncoder())
                ]
             )
             
@@ -87,6 +86,18 @@ class DataTranformation:
             numerical_features = ["writing_score", "reading_score"]
             
             input_feature_train_df = train_df.drop(columns=target_column, axis=1)
+            target_feature_train_df = train_df[target_column]
+            
+            input_feature_test_df = test_df.drop(columns=target_column, axis=1)
+            target_feature_test_df = test_df[target_column]
+            
+           
+            
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+            
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
             
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path,
@@ -94,6 +105,7 @@ class DataTranformation:
             )
             
             logging.info("Saved preprocessed data.")
+            
         except Exception as e:
             raise CustomException(e, sys)
     
